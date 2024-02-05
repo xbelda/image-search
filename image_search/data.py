a2f5274f65fff176e159e68a4eae9df152f1958c
Xavier Belda <xaviblx@gmail.com>
2024-02-05 01:22:53 +0100

    Add ImagesDataset

--- image_search/data.py
+++ image_search/data.py
@@ -5,6 +5,48 @@ import torch
 from datasets import Dataset
 from PIL import Image
 
+from transformers import SiglipProcessor
+
+
+class ImagesDataset(torch.utils.data.Dataset):
+    """
+    Extract all available images from a `image_folder` directory.
+    Returns the name and the loaded image.
+    """
+
+    def __init__(self, image_folder: Path, processor: SiglipProcessor):
+        self.images_folder = image_folder
+        self.processor = processor
+
+        self.image_paths = sorted(image_folder.glob('*.jpg'))
+        self.names = [path.stem for path in self.image_paths]
+
+        self.ids_name = dict(enumerate(self.names))
+        self.name_ids = {name: idx for idx, name in self.ids_name.items()}
+
+    def id_to_name(self, idx: int) -> str:
+        return self.ids_name[idx]
+
+    def name_to_id(self, name: str) -> int:
+        return self.name_ids[name]
+
+    def __len__(self) -> int:
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        name = self.names[idx]
+        image_path = self.image_paths[idx]
+
+        idx = self.name_to_id(name)
+
+        image = Image.open(image_path).convert("RGB")
+        inputs = self.processor.image_processor(image, return_tensors="pt")
+
+        return {
+            "photo_id": idx,
+            "pixel_values": inputs.pixel_values.squeeze()
+        }
+
 
 class ConversionsDataset(torch.utils.data.Dataset):
     def __init__(self, data: pd.DataFrame, image_folder: Path):
