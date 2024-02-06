d4748c3e311dfe62ddb81e29367826def04889c8
Xavier Belda <xaviblx@gmail.com>
2024-02-06 21:33:18 +0100

    Add mean average precision and at k

--- image_search/metrics.py
+++ image_search/metrics.py
@@ -4,7 +4,7 @@ from typing import List, Dict
 
 
 def in_batch_recall_at_1(
-    predictions: torch.Tensor, targets: torch.Tensor
+        predictions: torch.Tensor, targets: torch.Tensor
 ) -> torch.Tensor:
     """
     Computes the recall at 1 metric for a batch of predictions.
@@ -22,7 +22,7 @@ def in_batch_recall_at_1(
 
 
 def hit_rate(
-    true_ids: np.ndarray, predicted_ids: np.ndarray, k: int | List[int]
+        true_ids: np.ndarray, predicted_ids: np.ndarray, k: int | List[int]
 ) -> Dict[int, float]:
     """
     Computes the hit rate of a batch of predictions for different values of k.
@@ -46,4 +46,52 @@ def hit_rate(
         hit_k = (predicted_ids_k == true_ids.T).any(axis=1).mean()
 
         score[current_k] = hit_k
+    return score
+
+
+def mean_average_precision(true_ids: np.ndarray, predicted_ids: np.ndarray) -> float:
+    """
+    Computes the average precision of a batch of predictions.
+    Args:
+        true_ids: Array containing the true labels for the batch.
+        predicted_ids: Array containing the predicted labels for the batch.
+
+    Returns:
+        Average_precision.
+
+    """
+
+    matches = predicted_ids == true_ids.T
+
+    # A is relevance
+    precision = matches.cumsum(axis=1) / (1 + np.arange(matches.shape[1]))
+    masked_precision = precision * matches
+
+    num_correct = matches.sum(axis=1) + 1e-9  # Add epsilon to avoid division by zero
+
+    _average_precision = masked_precision.sum(axis=1) / num_correct
+    return _average_precision.mean()
+
+
+def mean_average_precision_at_k(true_ids: np.ndarray, predicted_ids: np.ndarray, k: int | List[int]) -> Dict[int, float]:
+    """
+    Computes the average precision of a batch of predictions for different values of k.
+    Args:
+        true_ids: Array containing the true labels for the batch.
+        predicted_ids: Array containing the predicted labels for the batch.
+        k: Array of different cutoff values for which to compute the hit rate.
+          If an integer is provided, computes only that value.
+
+    Returns:
+        Dictionary containing the average_precision at different values of k.
+    """
+
+    if isinstance(k, int):
+        k = [k]
+
+    score = {}
+    for current_k in k:
+        predicted_ids_k = predicted_ids[:, :current_k]
+        map_at_k = mean_average_precision(true_ids, predicted_ids_k)
+        score[current_k] = map_at_k
     return score
--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,6 +1,7 @@
 import numpy as np
+import pytest
 
-from image_search.metrics import hit_rate
+from image_search.metrics import hit_rate, mean_average_precision, mean_average_precision_at_k
 
 
 class TestHitRate:
@@ -45,3 +46,51 @@ class TestHitRate:
         expected = {1: 0.0, 2: 0.0}
 
         assert actual == expected
+
+
+class TestMeanAveragePrecision:
+    @pytest.mark.parametrize(
+        "true_ids, predicted_ids, expected",
+        [
+            (np.array([[1]]), np.array([[1, 2, 3]]), 1.0),
+            (np.array([[2]]), np.array([[1, 2, 3]]), 0.5),
+            (np.array([[4]]), np.array([[1, 2, 3]]), 0.0),
+            (
+                    np.array([[1, 2, 3]]),
+                    np.array([
+                        [1, 2, 3, 4, 5],
+                        [1, 2, 10, 11, 12],
+                        [6, 7, 8, 9, 10],
+                    ]),
+                    0.5  # (1+0.5+0) / 3
+            ),
+        ],
+        ids=[
+            "Match in first position",
+            "Match in second position",
+            "No match",
+            "Complex matches",
+        ]
+    )
+    def test_simple_examples_return_expected(self, true_ids, predicted_ids, expected):
+        actual = mean_average_precision(true_ids=true_ids, predicted_ids=predicted_ids)
+
+        assert np.allclose(actual, expected, atol=1e-5)
+
+
+class TestMeanAveragePrecisionAtK:
+    def test_simple_examples_return_expected(self):
+        true_ids = np.array([[1, 2, 3, 4]])
+        predicted_ids = np.array([
+            [1, 2, 3, 4, 5],
+            [1, 2, 10, 11, 12],
+            [6, 7, 8, 9, 10],
+            [6, 7, 8, 4, 10],
+        ])
+
+        k = [1, 2, 5]
+        actual = mean_average_precision_at_k(true_ids=true_ids, predicted_ids=predicted_ids, k=k)
+        expected = {1: 1 / 4, 2: (1 + 0.5) / 4, 5: (1.0 + 0.5 + 0.25) / 4}
+
+        for current_k in k:
+            assert np.allclose(actual[current_k], expected[current_k], atol=1e-5)
